d140d503531bdfd7d632d5f1153d340d75844a8e
Sam Freedgood <sam@samfreedgood.dev>
2025-08-19 16:10:23 +0200

    [feat](api & devx): introduce basic endpoint and improve icon component
    **Why**
    
    Connect frontend to backend.
    
    Better naming and implementation experience for using icons
    
    **What**
    
     - MVP api consumption
    
     - Define icon names and map to files by name

--- apps/async-games-frontend/src/components/Icon.tsx
+++ apps/async-games-frontend/src/components/Icon.tsx
@@ -2,22 +2,30 @@ import React from 'react';
 
 import * as Icons from '../assets/svgs';
 
-const icons = {
-  spade: Icons.Spade,
-  heart: Icons.Heart,
-  diamond: Icons.Diamond,
-  club: Icons.Club,
-  joker: Icons.Joker,
+export const iconNames = {
+  spade: 'spade',
+  heart: 'heart',
+  diamond: 'diamond',
+  club: 'club',
+  joker: 'joker',
 } as const;
 
-interface IconProps extends React.SVGProps<SVGSVGElement> {
-  icon: keyof typeof icons;
+const iconSVG = {
+  [iconNames.spade]: Icons.Spade,
+  [iconNames.heart]: Icons.Heart,
+  [iconNames.diamond]: Icons.Diamond,
+  [iconNames.club]: Icons.Club,
+  [iconNames.joker]: Icons.Joker,
+} as const;
+
+export interface IconProps extends React.SVGProps<SVGSVGElement> {
+  icon: keyof typeof iconSVG;
   height?: number;
   width?: number;
 }
 
 const Icon: React.FC<IconProps> = ({ icon, height, width }: IconProps) => {
-  return <img height={height} width={width} src={icons[icon]} />;
+  return <img height={height} width={width} src={iconSVG[icon]} />;
 };
 
 export default Icon;
