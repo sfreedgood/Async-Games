9ec0554ada7362887b7ffb7bb99b5075f69cc4a9
Sam Freedgood <sam@samfreedgood.dev>
2025-10-23 12:01:15 +0200

    [refactor](game ui): extract player from table to own component
    **Why**
    
    Better component isolation and Single Responsibility Principle.

--- apps/async-games-frontend/src/components/Table.stories.ts
+++ apps/async-games-frontend/src/components/Table.stories.ts
@@ -1,18 +1,50 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import { Table } from './Table';
+
 // import { expect } from 'storybook/test';
 
 const meta = {
   component: Table,
   title: 'Table',
+  args: {
+    players: [{ name: 'Player 1', id: '12345' }],
+  },
 } satisfies Meta<typeof Table>;
 export default meta;
 
 type Story = StoryObj<typeof Table>;
 
-export const Primary = {
-  args: {},
-} satisfies Story;
+export const SinglePlayer = {} satisfies Story;
+
+export const TwoPlayers = {
+  args: {
+    players: [
+      { name: 'Player 1', id: '12345' },
+      { name: 'Player 2', id: '23456' },
+    ],
+  },
+};
+
+export const ThreePlayers = {
+  args: {
+    players: [
+      { name: 'Player 1', id: '12345' },
+      { name: 'Player 2', id: '23456' },
+      { name: 'Player 3', id: '34567' },
+    ],
+  },
+};
+
+export const FourPlayers = {
+  args: {
+    players: [
+      { name: 'Player 1', id: '12345' },
+      { name: 'Player 2', id: '23456' },
+      { name: 'Player 3', id: '34567' },
+      { name: 'Player 4', id: '45678' },
+    ],
+  },
+};
 
 // export const Heading = {
 //   args: {},
