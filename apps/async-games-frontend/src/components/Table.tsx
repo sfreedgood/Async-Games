0c67a7aa909c844e4abfac6da34e903e850a9e12
Sam Freedgood <sam@samfreedgood.dev>
2025-10-23 12:01:15 +0200

    [refactor](game ui): extract player from table to own component
    **Why**
    
    Better component isolation and Single Responsibility Principle.

--- apps/async-games-frontend/src/components/Table.tsx
+++ apps/async-games-frontend/src/components/Table.tsx
@@ -1,40 +1,27 @@
 import React from 'react';
+import { PlayerAvatar } from './PlayerAvatar';
+import type { Player } from '../entities/player';
 
 export interface TableProps {
-  players: string[];
-  pieces: any; // cards, board, etc.
+  players: Player[];
+  pieces?: any; // cards, board, etc.
 }
 
 export const Table: React.FC<TableProps> = ({
   players,
   pieces,
 }: TableProps) => {
-  const playerCount = players.length;
-  const isSquare = playerCount <= 4;
-
-  const tableStyleRound = `flex justify-center items-center w-100 h-100 rounded-full border-2 border-black relative`;
-  const tableStyleSquare = `flex justify-center items-center w-100 h-100 rounded-none`;
-
-  const playerStyle = `absolute w-12 h-12 flex justify-center items-center 
-    border border-blue-500 rounded-full`;
-
-  const getPlayerPosition = (index: number) => {
-    const angle = (index / playerCount) * 2 * Math.PI;
-    const radius = isSquare ? 150 : 180; // Adjust radius for circle
-    const x = radius * Math.cos(angle);
-    const y = radius * Math.sin(angle);
-    return { left: `calc(50% + ${x}px)`, top: `calc(50% + ${y}px)` };
-  };
-
   return (
-    <div className={isSquare ? tableStyleSquare : tableStyleRound}>
+    <div
+      className={`grid grid-cols-6 grid-rows-6 place-items-stretch place-content-stretch bg-green-300 h-screen w-full`}
+    >
       {players.map((player, index) => (
-        <div
-          key={index}
-          className={`${playerStyle} ${getPlayerPosition(index)}`}
-        >
-          {player}
-        </div>
+        <PlayerAvatar
+          key={`${player.name}-${index}`}
+          player={player}
+          position={index}
+          totalPlayers={players.length}
+        />
       ))}
       {/* Render pieces (cards, board, etc.) here */}
     </div>
